3af382b49c3a402b7d2c6dda96017ab8a06f67bf
Douglas Sales Amante <douglassales@gec.inatel.br>
2018-06-07 20:09:44 -0300

    Update Teams.java

--- ProbabilisticTactics/src/br/inatel/EC205/model/Teams.java
+++ ProbabilisticTactics/src/br/inatel/EC205/model/Teams.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.inatel.EC205.model;
 
 /**
  *
- * @author casasbahia
+ * @author amantedouglas
  */
 public class Teams {
     
